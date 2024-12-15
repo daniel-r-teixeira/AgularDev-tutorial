cc950ed47364960c0f3eac68f0f72ebdb8ed0ae9
daniel-r-teixeira <daniel.r.teixeira@claranet.com>
2024-12-15 23:31:24 +0000

    Create Home component - css

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,12 +7,12 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
   standalone: true,
   imports: [CommonModule, HousingLocationComponent],
   template: `
-    <section>
-      <form>
-        <input type="text" placeholder="Filter by city">
-        <button class="primary" type="button">Search</button>
-      </form>
-    </section>
+  <section>
+    <form>
+      <input type="text" placeholder="Filter by city" />
+      <button class="primary" type="button">Search</button>
+    </form>
+  </section>
 
     <section class="results">
       <app-housing-location></app-housing-location>
