711bb33935fe6eb19565c67e12484c762dc903ca
daniel-r-teixeira <daniel.r.teixeira@claranet.com>
2024-12-16 03:08:56 +0000

    Add HTTP communication to your app

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -59,8 +59,10 @@ export class DetailsComponent {
   })
 
   constructor () {
-    const housingLocarionId = Number(this.route.snapshot.params["id"])
-    this.housingLocation = this.housingService.getHousingLocationById(housingLocarionId);
+    const housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
+    this.housingService.getHousingLocationById(housingLocationId).then((housingLocation) => {
+      this.housingLocation = housingLocation;
+    });
   }
 
   submitApplication(){
