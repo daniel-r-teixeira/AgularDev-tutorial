b199e4e118650e1e2c6e0a5c8065c127482bbead
daniel-r-teixeira <daniel.r.teixeira@claranet.com>
2024-12-16 02:38:49 +0000

    Add the search feature to your app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,26 @@
 import { Component } from '@angular/core';
+// components
 import { HomeComponent } from './home/home.component';
+// route
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   template: `
     <main>
-      <header class = "brand-name">
-        <img class = "brand-logo" src = "/assets/logo.svg" alt = "logo" aria-hidden = "true" >
-      </header>
-
+      <a [routerLink]="['/']">
+        <header class = "brand-name">
+          <img class = "brand-logo" src = "/assets/logo.svg" alt = "logo" aria-hidden = "true" >
+        </header>
+      </a>
       <section class="content">
-        <app-home></app-home>
+        <router-outlet></router-outlet>
       </section>
     </main>
   `,
   styleUrls: ['./app.component.css'],
-  imports: [HomeComponent]
+  imports: [HomeComponent,RouterModule]
 })
 export class AppComponent {
   title = 'homes';
--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 // routes
 import { ActivatedRoute } from '@angular/router';
 // services
@@ -10,30 +11,40 @@ import { HousingLocation } from '../housing-location';
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
-    <p>
-      details works! {{housingLocation?.id}}
-    </p>
-    <article>
-      <img class="listing-photo" [src]="housingLocation?.photo">
-      <section class="listing-description">
-        <h2 class="listing-heading">{{housingLocation?.name}}</h2>
-        <p class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}</p>
-      </section>
-      <section class="listing-features">
-        <h2 class="listing-heading">About this housing location</h2>
-        <ul>
-          <li>Units availabe: {{housingLocation?.availableUnits}}</li>
-          <li>Does this location have wifi: {{housingLocation?.wifi}}</li>
-          <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
-        </ul>
-      </section>
-      <section class="listing-apply">
-        <h2 class="listing-heading">Apply now to live here</h2>
-        <button class"primary" type="button">Apply now</button>
-      </section>
-    </article>
+  <article>
+    <img
+      class="listing-photo"
+      [src]="housingLocation?.photo"
+      alt="Exterior photo of {{ housingLocation?.name }}"
+      crossorigin
+    />
+    <section class="listing-description">
+      <h2 class="listing-heading">{{ housingLocation?.name }}</h2>
+      <p class="listing-location">{{ housingLocation?.city }}, {{ housingLocation?.state }}</p>
+    </section>
+    <section class="listing-features">
+      <h2 class="section-heading">About this housing location</h2>
+      <ul>
+        <li>Units available: {{ housingLocation?.availableUnits }}</li>
+        <li>Does this location have wifi: {{ housingLocation?.wifi }}</li>
+        <li>Does this location have laundry: {{ housingLocation?.laundry }}</li>
+      </ul>
+    </section>
+    <section class="listing-apply">
+      <h2 class="section-heading">Apply to live here</h2>
+      <form [formGroup]="applyForm" (submit)="submitApplication()">
+          <label for="first-name">First Name</label>
+          <input id="first-name" type="text" formControlName="firstName" />
+          <label for="last-name">Last Name</label>
+          <input id="last-name" type="text" formControlName="lastName" />
+          <label for="email">Email</label>
+          <input id="email" type="email" formControlName="email" />
+          <button type="submit" class="primary">Apply now</button>
+        </form>
+    </section>
+  </article>
   `,
   styleUrls: ['./details.component.css']
 })
@@ -41,9 +52,22 @@ export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  })
 
   constructor () {
     const housingLocarionId = Number(this.route.snapshot.params["id"])
     this.housingLocation = this.housingService.getHousingLocationById(housingLocarionId);
+  }
+
+  submitApplication(){
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    );
   }
 }
